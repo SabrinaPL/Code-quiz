6092f84170f7f5b1b241074c23e8e9b31c984ee5
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2023-11-23 15:16:47 +0100

    Update of nickname-form component

--- src/js/components/nickname-form/nickname-form.js
+++ src/js/components/nickname-form/nickname-form.js
@@ -5,6 +5,30 @@
  * @version 1.1.0
  */
 
+const template = document.createElement('template')
+template.innerHTML = `
+<div id="nickname-form">
+<form>
+<label for="playerName">Enter a nickname to start the quiz:</label><br>
+<input type="text" id="playerName" name="playerName" required><br>
+</form><br>
+<div id="btn">
+<button id="submit">Let's play!</button>
+</div>
+</div>
+<style>
+#submit {
+background-color: #FF66B3; 
+color: white; 
+padding: 5px; 
+border-radius: 5px; 
+}
+#submit:active {
+background-color: #42BFDD;  
+}
+</style>
+`
+
 /** Nickname form class that extends HTMLElement class
  * and creates a custom element.
  */
@@ -21,7 +45,7 @@ class nicknameForm extends HTMLElement {
    *
    * @type {string}
    * */
-  #nickname = ''
+  #nickname
 
   /**
    * Constructor for nicknameForm class which invokes its super class constructor.
@@ -29,35 +53,12 @@ class nicknameForm extends HTMLElement {
   constructor () {
     super()
 
-    const template = document.createElement('template')
-    template.innerHTML = `
-<div id="nickname-form">
-  <form>
-    <label for="playerName">Enter a nickname to start the quiz:</label><br>
-    <input type="text" id="playerName" name="playerName"><br>
-  </form><br>
-  <div id="btn">
-    <button id="submit">Let's play!</button>
-  </div>
-</div>
-<style>
-    #submit {
-    background-color: #FF66B3; 
-    color: white; 
-    padding: 5px; 
-    border-radius: 5px; 
-  }
-    #submit:active {
-    background-color: #42BFDD;  
-  }
-</style>
-`
     // Attach a shadow DOM tree to this element and
     // append the template to the shadow root.
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.append(template.content.cloneNode(true))
 
-    // Get the nickname form element in the shadow root. Should I use getters and setters here instead, for better encapsulation?
+    // Get the nickname form element in the shadow root.
     this.#nicknameForm = this.shadowRoot.querySelector('#nickname-form')
 
     // Set up event handler for the nickname form.
@@ -67,16 +68,13 @@ class nicknameForm extends HTMLElement {
       // I want to prevent the browsers default behaviour here, so that the form doesn't submit (and refresh the webpage).
       event.preventDefault()
 
-      // After the nickname has been stored, the nickname-form component should be removed from the DOM (next step? - communicate with next component, the quiz-question, and add it to the DOM to start the game).
-      this.#nicknameForm.remove()
+      console.log(this.#nickname)
+
+      // Dispatch event to communicate with quiz-application - quiz-application will listen for this event, store the nickname in a nickname property, then remove the nickname-form component from the DOM?
+      this.#nicknameForm.dispatchEvent(new CustomEvent('nickname', { detail: this.#nickname }))
     })
   }
 }
-
-// Should I use callbacks?
-// Observers?
-// Dispatch events, to communicate with other components?
-// I want to send the stored nickname to the high-score component, so that it can be displayed to the user once the quiz has finished.
 
 // Define the custom element.
 customElements.define('nickname-form', nicknameForm)
